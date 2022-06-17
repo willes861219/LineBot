2bb57a5320277a0a27fe02c7e776b90a1aac1da2
willes <wilkes861219@gmail.com>
2022-06-17 08:58:04 +0800

    '標記內容'

--- main.py
+++ main.py
@@ -38,5 +38,5 @@ def handle_message(event):
 
 import os
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT',5000))
-    app.run(host='localhost', port=port)
+    port = int(os.environ.get('PORT',80))
+    app.run(host='0.0.0.0', port=port)
