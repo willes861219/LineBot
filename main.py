cb0e5a33eddc4ced511a5599fa606e88387c70c2
willes <wilkes861219@gmail.com>
2022-06-20 15:45:36 +0800

    '新增判斷與移除鸚鵡式回覆'

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from linebot.models import *
 app = Flask(__name__)
 # LINE BOT info
 line_bot_api = LineBotApi('Oab2kpZ3f0t35+8oYNfTpYbq9T4taRyVminiW9gHGUAbgnWfiWPpUoqmn2LpXEySzWu33oZgZQNY3xHDE67nH6+spvtyxzy7OZy+F3y8LqHYXHPZM7qJenb7ULux0oOcXLbn9Lg5D8oRzfm8ic8NBAdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('aff823673f1d48c14b2875b853ebb17f')
+handler = WebhookHandler('aff823673f1d48c14b2875b853ebb17f') 
 
 # 指定在 /callback 通道上接收訊息，且方法是 POST，而callback()是為了要檢查連線是否正常
 @app.route("/callback", methods=['POST'])
@@ -24,7 +24,7 @@ def callback():
     print(body)
     try:
         handler.handle(body, signature)
-    except InvalidSignatureError:
+    except InvalidSignatureError:  
         abort(400)
     return 'OK'
 
@@ -36,37 +36,62 @@ def handle_message(event):
     user_id = event.source.user_id
     reply_token = event.reply_token
     message = event.message.text
-    if(message == '嗨'):
-        text_message = TextSendMessage(text = 'No嗨')
+    profile = line_bot_api.get_profile(user_id) #取得使用者資訊 
+    #{
+    #   "userId": "U82******************",
+    #   "displayName": "張君祥", // 傳訊息使用者的帳號名字
+    #   "pictureUrl": "https://sprofile.line-scdn.net/***/", // 使用者的大頭照圖片網址
+    #   "statusMessage": "蘋果仁 IG 編輯\n做個無所畏懼的謙卑之人",  // 使用者的 Bio
+    #   "language": "zh-Hant" // 使用者的偏好語言
+    #}
+    print(profile.display_name, "：",message)
+    if("嗨" in message or "安安" in message ):
+        text_message = TextSendMessage(text = profile.display_name + ",你好")
         line_bot_api.reply_message(reply_token, text_message)
-    elif(message == "保重"):
-        text_message = TextSendMessage(text = '謝謝你的關心')
-        line_bot_api.reply_message(reply_token, text_message)
-    elif(message == '貼圖'):
-        sticker_message = StickerSendMessage(package_id='6325',sticker_id='10979907')
+    elif(message == '貼圖一'):
+        num = random.randint(76,99)
+        exportNum = '105513'+str(num)
+        sticker_message = StickerSendMessage(package_id='6136',sticker_id=exportNum)
         line_bot_api.reply_message(reply_token, sticker_message)
-    elif(message == '圖片'):
-        image_message = ImageSendMessage(original_content_url='https://file-examples-com.github.io/uploads/2017/10/file_example_PNG_500kB.png',preview_image_url='https://file-examples-com.github.io/uploads/2017/10/file_example_PNG_500kB.png')
-        line_bot_api.reply_message(reply_token, image_message)
-    elif(message == '影片'):
-        video_message = VideoSendMessage(original_content_url='https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_480_1_5MG.mp4',preview_image_url='https://file-examples-com.github.io/uploads/2017/10/file_example_PNG_500kB.png')
-        line_bot_api.reply_message(reply_token, video_message)
-    elif(message == '聲音'):
-        audio_message = AudioSendMessage(original_content_url='https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3',duration=3000)
-        line_bot_api.reply_message(reply_token, audio_message)
-    elif(message == 'Location'):
-        location_message = LocationSendMessage(title='my location',address='Tokyo',latitude=35.65910807942215,longitude=139.70372892916203)
-        line_bot_api.reply_message(reply_token, location_message)
+    elif(message == '貼圖二'):
+        num = random.randint(4,24) 
+        exportNum = '109799'+str(num)
+        sticker_message = StickerSendMessage(package_id='6325',sticker_id=exportNum)
+        line_bot_api.reply_message(reply_token, sticker_message)
+    # elif(message == '圖片'):
+    #     image_message = ImageSendMessage(original_content_url='https://file-examples-com.github.io/uploads/2017/10/file_example_PNG_500kB.png',preview_image_url='https://file-examples-com.github.io/uploads/2017/10/file_example_PNG_500kB.png')
+    #     line_bot_api.reply_message(reply_token, image_message)
+    # elif(message == '影片'):
+    #     video_message = VideoSendMessage(original_content_url='https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_480_1_5MG.mp4',preview_image_url='https://file-examples-com.github.io/uploads/2017/10/file_example_PNG_500kB.png')
+    #     line_bot_api.reply_message(reply_token, video_message)
+    # elif(message == '聲音'):
+    #     audio_message = AudioSendMessage(original_content_url='https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3',duration=3000)
+    #     line_bot_api.reply_message(reply_token, audio_message)
+    # elif(message == 'Location'):
+    #     location_message = LocationSendMessage(title='my location',address='Tokyo',latitude=35.65910807942215,longitude=139.70372892916203)
+    #     line_bot_api.reply_message(reply_token, location_message)
+    elif('爛' in message or '幹' in message or '垃圾' in message):
+        text_message = TextSendMessage(text = profile.display_name + ',你講話可不可以不要這麼臭')
+        line_bot_api.reply_message(reply_token, text_message)
+    elif('謝' in message):
+        text_message = TextSendMessage(text = '不客氣')
+        line_bot_api.reply_message(reply_token, text_message)
+    elif('對不起' in message):
+        text_message = TextSendMessage(text = profile.display_name + ',我原諒你')
+        line_bot_api.reply_message(reply_token, text_message)
+    elif('恐怖' in message):
+        text_message = TextSendMessage(text = profile.display_name + ',沒你長的恐怖')
+        line_bot_api.reply_message(reply_token, text_message)
     elif(message == '早餐'):
         FlexMessage = json.load(open('burger.json','r',encoding='utf-8'))
         line_bot_api.reply_message(reply_token, FlexSendMessage('123',FlexMessage))
-    elif(message == '抽籤'):
+    elif(message == '抽籤' or '我抽籤' in message):
         result =  TextSendMessage(random.choice(['大吉','中吉','小吉','吉','末吉','凶','大凶']))
         line_bot_api.reply_message(reply_token, result)
-    line_bot_api.reply_message(reply_token, TextSendMessage(text = "我不知道你在說什麼，你可以輸入\"早餐\"or\"抽籤\""))
-    print(message)
-
+    elif('已讀' in message):
+        text_message = TextSendMessage(text = '絕對不是我已讀的')
+        line_bot_api.reply_message(reply_token, text_message)
 import os
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT',80))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT',8080))
+    app.run(host='localhost', port=port)
