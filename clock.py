f313a69cc1c878da6732d5b614344e31032d89b3
willes <wilkes861219@gmail.com>
2022-06-23 08:45:10 +0800

    'update'

--- clock.py
+++ clock.py
@@ -1,11 +1,19 @@
+import datetime
 import urllib.request 
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', second="*/5")
+@sched.scheduled_job('cron', day_of_week='mon-fri', minutes='*/1')
 def scheduled_job():
+    print('========== APScheduler CRON =========')
+    # 馬上讓我們瞧瞧
+    print('This job runs every day */2 min.')
+    # 利用datetime查詢時間
+    print(f'{datetime.datetime.now().ctime()}')
+    print('========== APScheduler CRON =========')
+
     url = "https://yukibot-test.herokuapp.com/"
     conn = urllib.request.urlopen(url)
 
