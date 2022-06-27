2f2e58e9476303f76501ccc933ee6b42a576e23a
willes <wilkes861219@gmail.com>
2022-06-27 14:02:33 +0800

    新增重置抽籤次數

--- resetDrawStraws.py
+++ resetDrawStraws.py
@@ -7,7 +7,7 @@ sched = BlockingScheduler()
 
 
 #利用scheduled_job()這個函數的第一個參數'cron'，告訴 Python，當幾年幾月幾日幾點幾分幾秒的時候，總而言之就是特定時間，執行下述程式碼。
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour='14')
+@sched.scheduled_job('cron', day_of_week='mon-sun', minute='*/2')
 def scheduled_job():
     print('========== 重置抽籤次數 =========')
     print('這個工作在每天的晚上12點執行')
