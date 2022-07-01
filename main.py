83612565f8cf7099d03bc207e2d1df2c20e03023
willes <wilkes861219@gmail.com>
2022-07-01 18:05:13 +0800

    update

--- main.py
+++ main.py
@@ -279,5 +279,5 @@ def handle_message(event):
         line_bot_api.reply_message(reply_token, text_message)
 import os
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT',8080))
-    app.run(host='localhost', port=port)
+    port = int(os.environ.get('PORT',80))
+    app.run(host='0.0.0.0', port=port)
