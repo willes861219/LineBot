784b7c57294971c478f7d1175f4989cb608f8ffc
willes <wilkes861219@gmail.com>
2022-07-27 11:54:37 +0800

    'update'

--- main.py
+++ main.py
@@ -296,5 +296,6 @@ def handle_message(event):
 
 import os
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT',8080))
-    app.run(host='0.0.0.0', port=port,debug=True)
+    app.run()
+    # port = int(os.environ.get('PORT',8080))
+    # app.run(host='0.0.0.0', port=port,debug=True)
